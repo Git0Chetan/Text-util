fc36b74713619e30ecb51d81d8210df1b8211185
chetan <chetan248581@gmail.com>
2023-06-10 12:31:38 +0530

    Added Dark mode to the Body of Website tut-12

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import React ,{useState} from 'react';
 
 function App() {
+  const[mode,setMode]=useState('light');
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor="rgb(4,4,56)";
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor="white";
+      
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" about="About_T"/>
-      <div className="container my.3">
-        <About/>
-        <TextForm heading="Enter the text Below to Analyze "/>
+      <Navbar title="TextUtils" about="About_T" mode={mode} toggleMode={toggleMode}/>
+      <div className="container my.4">
+        {/* <About/> */}
+        <TextForm heading="Enter the text Below to Analyze " mode={mode}/>
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -30,23 +30,30 @@ export default function TextForm(props) {
         setText(event.target.value);
         // setcount(event.target.value.length);
     }
+
+    const handlecopy=()=>{
+      var text=document.getElementById("myBox");
+      text.select();
+      navigator.clipboard.writeText(text.value);
+    }
     
 
   return (
     <>
     <div className='container'>
-        <h1>{props.heading}</h1>
+        <h1 style={{color:props.mode==='light'?'black':'white'}}>{props.heading}</h1>
         <div className="mb-3">
-        <textarea className="form-control" value={text} placeholder='Enter your text here.' onChange={handlechange} id="myBox" rows="10"></textarea>
+        <textarea className="form-control" style={{backgroundColor:props.mode==='light'?'white':'gray'}} value={text} placeholder='Enter your text here.' onChange={handlechange} id="myBox" rows="10"></textarea>
         {/* <p>The number of letters including space are:{count}</p> */}
         </div>
         <button className="btn btn-primary mx-2" onClick={handleupclick}>Convert to Uppercase</button>
         <button className="btn btn-primary mx-2" onClick={handleloclick}>Convert to Lowercase</button>
         <button className="btn btn-primary mx-2" onClick={handleclearclick}>Clear the field</button>
+        <button className="btn btn-primary mx-2" onClick={handlecopy}>Copy the text</button>
         {/* <button className="btn btn-primary" onClick={handlecountclick}>Count the letters</button> */}
     </div>
 
-    <div className='container my-3'>
+    <div style={{color:props.mode==='light'?'black':'white'}} className='container my-3'>
       <h1>Here is the text summary</h1>
       <p>{text.split(" ").length} words and {text.length} characters</p>
       <p>{0.008*text.split(" ").length} time required to read</p>
