e1bf569f7c84703ec8651c50464c1df781f06ff0
chetan <chetan248581@gmail.com>
2023-06-10 10:21:42 +0530

    added more functionalities to application tut-8+9

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,28 +2,55 @@ import React,{useState} from 'react'
 // import PropTypes from 'prop-types'
 
 export default function TextForm(props) {
-    const[text, setText]=useState('Enter text here');
+    const[text, setText]=useState('');
     // text="wrong way to set text"
-    // setText("new text");
+    // setText("new text"); right way
+    // const[count, setcount]=useState(0);
 
-    const handleclick=()=>{
-        console.log("Uppercase was clicked"+text);
+    const handleupclick=()=>{
+        // console.log("Uppercase was clicked"+text);
         let newText=text.toUpperCase();
         setText(newText);
     }
-    const handlechange=(event)=>{
-        console.log("On change");
-        setText(event.target.value);
+    
+    const handleloclick=()=>{
+        let newText=text.toLowerCase();
+        setText(newText);
     }
+    const handleclearclick=()=>{
+        let newText="";
+        setText(newText);
+    }
+  //   const handlecountclick=()=>{
+  //     let newText=text.length;
+  //     setText(newText);
+  //  }
+    const handlechange=(event)=>{
+        // console.log("On change");
+        setText(event.target.value);
+        // setcount(event.target.value.length);
+    }
+    
 
   return (
-    <div>
+    <>
+    <div className='container'>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-        {/* <label for="myBox" class="form-label">Example textarea</label> */}
-        <textarea className="form-control" value={text} onChange={handlechange} id="myBox" rows="10"></textarea>
+        <textarea className="form-control" value={text} placeholder='Enter your text here.' onChange={handlechange} id="myBox" rows="10"></textarea>
+        {/* <p>The number of letters including space are:{count}</p> */}
         </div>
-        <button className="btn btn-primary" onClick={handleclick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleupclick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleloclick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleclearclick}>Clear the field</button>
+        {/* <button className="btn btn-primary" onClick={handlecountclick}>Count the letters</button> */}
     </div>
+
+    <div className='container my-3'>
+      <h1>Here is the text summary</h1>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008*text.split(" ").length} time required to read</p>
+    </div>
+  </>   
   )
 }
