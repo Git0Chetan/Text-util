76d1abbe69318d07c3d8a3e629ef2548b99b7998
chetan <chetan248581@gmail.com>
2023-06-12 10:34:57 +0530

    modified nav,json.file,start to deploy

--- src/components/About.js
+++ src/components/About.js
@@ -33,36 +33,36 @@ export default function About() {
         <div className="accordion-item " >
             <h2 className="accordion-header">
             <button className="accordion-button" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                Accordion Item #1
+                Convert to Uppercase
             </button>
             </h2>
             <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
             <div className="accordion-body">
-                <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                <strong>Try uppercase feature</strong> This text-to-uppercase feature provides users with a convenient way to change the case of text, ensuring consistent and uniform styling. It can be particularly useful when dealing with titles, headings, or any text that requires uppercase formatting.
             </div>
             </div>
         </div>
         <div className="accordion-item">
             <h2 className="accordion-header">
             <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Accordion Item #2
+                Convert to lowercase
             </button>
             </h2>
             <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
             <div className="accordion-body">
-                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                <strong>Try lowercase feature .</strong> The "Convert to Lowercase" feature enables users to transform text entered into the app into lowercase letters. This functionality is particularly useful when users want to standardize the case of their text or when dealing with case-insensitive operations.
             </div>
             </div>
         </div>
         <div className="accordion-item">
             <h2 className="accordion-header">
             <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Accordion Item #3
+                Count
             </button>
             </h2>
             <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
             <div className="accordion-body">
-                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                <strong>Count words and letters</strong> The "Counting Words and Letters" feature provides a valuable tool for users to understand the composition of their text. As users input their text, whether it's a paragraph, an essay, or even just a sentence, the app dynamically calculates and displays the number of words and letters in real-time.
             </div>
             </div>
         </div>
